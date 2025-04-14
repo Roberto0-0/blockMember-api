3e5ba54ed6057ed4de8cd53059edc27f2e4aff81
Robert <roberto88526@gmail.com>
2025-04-14 11:30:42 -0300

    responsible only for creating a group.

--- src/services/group/create.js
+++ src/services/group/create.js
@@ -1,27 +1,12 @@
 const { Group } = require("../../models/Group")
 
 class GroupCreate {
-    constructor(groupGetBySession, groupSaveChanges) {
-        this._groupGetBySession = groupGetBySession
-        this._groupSaveChanges = groupSaveChanges 
-    }
+    execute(groupProps) {
+        const { session, name } = groupProps
 
-    async execute(session, request) {
-        const { serialized, name } = request
-        const group = await this._groupGetBySession.execute(session)
-        if (group.success) return {
-            success: false,
-            message: "Este grupo já está criado."
-        }
+        const newGroup = new Group(session, name)
 
-        const newGroup = new Group(session, serialized, name)
-        await this._groupSaveChanges.execute(session, newGroup)
-
-        return {
-            success: true,
-            message: "Grupo criado com sucesso.",
-            data:  newGroup
-        }
+        return { data: newGroup }
     }
 }
 
