a8eca02dd2c96d0609efc5b8c5e2a7e17012d3b6
Robert <roberto88526@gmail.com>
2025-04-14 11:31:17 -0300

    code optimization.

--- src/services/group/getBySession.js
+++ src/services/group/getBySession.js
@@ -8,17 +8,16 @@ class GroupGetBySession {
     async execute(session) {
         const _storage = `${this._storage}/${session}.json`
 
-        if(!existsSync(_storage)) return {
+        if (!existsSync(_storage)) return {
             success: false,
             message: "O grupo não foi encontrado."
         }
 
-        const data = readFileSync(_storage, "utf8")
-        const content = JSON.parse(data)
+        const data = JSON.parse(readFileSync(_storage, "utf8"))
 
         return {
             success: true,
-            data: content
+            data
         }
     }
 }
